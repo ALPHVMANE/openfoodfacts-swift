28f80fd7ac970e75e51b63edac056b4573e8394d
Mel Tran <alphxrizen@gmail.com>
2025-11-24 07:57:05 +0000

    CardColorChange

--- Sources/ViewModifiers/ViewModifiers.swift
+++ Sources/ViewModifiers/ViewModifiers.swift
@@ -12,7 +12,7 @@ struct RoundedBackgroundCard: ViewModifier {
     
     func body(content: Content) -> some View {
         content
-            .padding(EdgeInsets(top: 20, leading: 10, bottom: 20, trailing: 10)).background(Color(red:95/255,green:105/255,blue:53/255)).cornerRadius(25)
+            .padding(EdgeInsets(top: 20, leading: 10, bottom: 20, trailing: 10)).background(Color(red: 26/255, green: 17/255, blue: 16/255)).cornerRadius(25)
     }
 }
 
